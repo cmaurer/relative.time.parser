7344b0071fa3e36a8e9f7581f2d97112fec7b88e
Christian Maurer <chris_p_maurer@yahoo.com>
2017-10-26 09:14:08 -0500

    cpm: adding codeclimate support

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,11 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+          build: ['build/'],
+          coverage: ['coverage/'],
+          contents: ['.nyc_output/*']
+        },
         jshint: {
             all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
             options: { jshintrc: '.jshintrc'}
@@ -20,24 +25,35 @@ module.exports = function(grunt) {
                 dest: 'build/relative.time.parser.min.js'
             }
         },
+        codeclimate: {
+          main: {
+            options: {
+              file: 'coverage/lcov.info',
+              token: 'a7bc1ef397115120c4c23d9361ae64475cd8797469cd6c2bcea7c57d70ca5e22',
+              executable: ''
+            }
+          }
+        },
         mochaTest: {
             test: {
                 options: {
                     reporter: 'spec',
-                    quiet: false
+                    quiet: false,
+                    require: 'blanket/blanket'
                 },
                 src: ['test/*.js']
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
-
-
+    grunt.loadNpmTasks('grunt-mocha-nyc');
+    grunt.loadNpmTasks('grunt-codeclimate-reporter');
     grunt.registerTask('test', ['mochaTest']);
-
-    grunt.registerTask('default', ['jshint', 'uglify', 'test']);
+    grunt.registerTask('codeclimate', ['codeclimate']);
+    grunt.registerTask('default', ['clean', 'jshint', 'uglify', 'test']);
 
 };
